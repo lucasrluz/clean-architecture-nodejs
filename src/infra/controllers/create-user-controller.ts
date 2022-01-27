488eb947b05277f85c3849cc9825b924b0295305
lucasrluz <lucasr.luzbr@gmail.com>
2022-01-27 13:01:50 -0300

    implementação do controller para criação de usuário

---
+++ src/infra/controllers/create-user-controller.ts
@@ -0,0 +1,19 @@
+import { CreateUserUseCaseInterface } from '../../app/useCases/interfaces/create-user-use-case-interface';
+import { User } from '../../domain/user';
+import { badRequest, created } from './util/response/responses';
+
+export class CreateUserController {
+  private readonly createUserUseCase: CreateUserUseCaseInterface;
+
+  constructor(createUserUseCase: CreateUserUseCaseInterface) {
+    this.createUserUseCase = createUserUseCase;
+  }
+
+  async perform(user: User) {
+    const response = await this.createUserUseCase.create(user);
+
+    if (response.isError()) return badRequest(response.value);
+
+    return created(response.value);
+  }
+}
