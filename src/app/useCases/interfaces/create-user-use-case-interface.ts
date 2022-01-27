aa6a3dde7d2d81b9a55c692d341f5a6e19311d7e
lucasrluz <lucasr.luzbr@gmail.com>
2022-01-27 12:52:36 -0300

    interface para criação de usuário no caso de uso

---
+++ src/app/useCases/interfaces/create-user-use-case-interface.ts
@@ -0,0 +1,7 @@
+import { User } from '../../../domain/user';
+
+export interface CreateUserUseCaseInterface {
+  create(
+    user: User,
+  ): Promise<{ value: any; isError(): boolean; isSuccess(): boolean }>;
+}
