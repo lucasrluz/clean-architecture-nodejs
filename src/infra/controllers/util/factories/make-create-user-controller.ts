2907877d1db73859a8b3a38e26202708a8f04b01
lucasrluz <lucasr.luzbr@gmail.com>
2022-01-27 13:01:15 -0300

    função para criação do controller

---
+++ src/infra/controllers/util/factories/make-create-user-controller.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client';
+import { CreateUserUseCase } from '../../../../app/useCases/create-user-use-case';
+import { Bcrypt } from '../../../bcrypt/bcrypt';
+import { PrismaUserRepository } from '../../../prisma/repositories/prisma-user-repository';
+import { CreateUserController } from '../../create-user-controller';
+
+export const makeCreateUserController = () => {
+  const prisma = new PrismaClient();
+  const userRepository = new PrismaUserRepository(prisma);
+  const encoder = new Bcrypt();
+  const createUserUseCase = new CreateUserUseCase(userRepository, encoder);
+  const createUserController = new CreateUserController(createUserUseCase);
+
+  return createUserController;
+};
