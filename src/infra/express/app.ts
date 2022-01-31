09f058a0a25623755e922fd28316099c340f5f92
lucasrluz <lucasr.luzbr@gmail.com>
2022-01-31 11:22:40 -0300

    habilitando rota de login

--- src/infra/express/app.ts
+++ src/infra/express/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { userRouter } from './routes/user-routes';
 import dotenv from 'dotenv';
+import { authenticationRouter } from './routes/authentication-routes';
 
 dotenv.config();
 
@@ -8,3 +9,4 @@ export const app = express();
 
 app.use(express.json());
 app.use(userRouter);
+app.use(authenticationRouter);
