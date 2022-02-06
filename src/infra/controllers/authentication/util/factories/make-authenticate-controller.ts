3c27cd19991b585635b6a1d14fcc84a5cc4e0c4c
lucasrluz <lucasr.luzbr@gmail.com>
2022-02-06 11:56:55 -0300

    inicialização do repositório do refresh token

--- src/infra/controllers/authentication/util/factories/make-authenticate-controller.ts
+++ src/infra/controllers/authentication/util/factories/make-authenticate-controller.ts
@@ -2,16 +2,19 @@ import { PrismaClient } from '@prisma/client';
 import { AuthenticateUseCase } from '../../../../../app/useCases/authentication/authenticate-use-case';
 import { Bcrypt } from '../../../../bcrypt/bcrypt';
 import { Jwt } from '../../../../jwt/jwt';
+import { PrismaRefreshTokenRepository } from '../../../../prisma/repositories/prisma-refresh-token-repository';
 import { PrismaUserRepository } from '../../../../prisma/repositories/prisma-user-repository';
 import { AuthenticateController } from '../../authenticate-controller';
 
 export const makeAuthenticateController = () => {
   const prisma = new PrismaClient();
   const userRepository = new PrismaUserRepository(prisma);
+  const refreshTokenRepository = new PrismaRefreshTokenRepository(prisma);
   const encoder = new Bcrypt();
   const jwt = new Jwt();
   const authenticateUseCase = new AuthenticateUseCase(
     userRepository,
+    refreshTokenRepository,
     encoder,
     jwt,
   );
