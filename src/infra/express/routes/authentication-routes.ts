d8ba50319b59722fd78d9f91b11e07ece67df672
lucasrluz <lucasr.luzbr@gmail.com>
2022-02-07 15:31:38 -0300

    rota para gerar novo refresh token

--- src/infra/express/routes/authentication-routes.ts
+++ src/infra/express/routes/authentication-routes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
 import { makeAuthenticateController } from '../../controllers/authentication/util/factories/make-authenticate-controller';
+import { makeGenerateNewRefreshTokenController } from '../../controllers/authentication/util/factories/make-generate-new-refresh-token-controller';
 import { adaptRoute } from './util/adapter/adaptRoute';
+import { generateNewRefreshTokenAdapRoute } from './util/adapter/generate-new-refresh-token-adap-route';
 
 export const authenticationRouter = Router();
 
 authenticationRouter.post('/login', adaptRoute(makeAuthenticateController()));
+authenticationRouter.post(
+  '/refresh_token',
+  generateNewRefreshTokenAdapRoute(makeGenerateNewRefreshTokenController()),
+);
