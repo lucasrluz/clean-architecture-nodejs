201ca482eb25d83233c07c91ae5eb8118b43ee5c
lucasrluz <lucasr.luzbr@gmail.com>
2022-02-08 12:59:49 -0300

    refatoração no formato de atribuição do nome do erro

--- src/app/useCases/user/util/errors/existing-user-error.ts
+++ src/app/useCases/user/util/errors/existing-user-error.ts
@@ -1,7 +1,8 @@
 export class ExistingUserError extends Error {
-  public readonly name = 'ExistingUserError';
+  public readonly name: string;
 
   constructor() {
     super('User already registered');
+    this.name = 'ExistingUserError';
   }
 }
