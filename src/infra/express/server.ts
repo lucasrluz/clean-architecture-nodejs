e496a1c937c6d8029790a522fecd45ebe77b5950
lucasrluz <lucasr.luzbr@gmail.com>
2022-02-14 09:44:50 -0300

    alteração no formato de configuração da porta do servidor

--- src/infra/express/server.ts
+++ src/infra/express/server.ts
@@ -1,3 +1,5 @@
 import { app } from './app';
 
-app.listen(3000, () => console.log('Server is running on PORT 3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
